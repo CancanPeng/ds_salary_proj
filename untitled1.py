5b4c5c94e180fe2dc6c15e702d3d66a0c8fd2656
CancanPeng <ccpeng0101@gmail.com>
2020-07-27 21:14:35 +0800

    push data_collection.py

---
+++ untitled1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 25 11:04:43 2020
+
+@author: pengcancan
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "/Users/pengcancan/Workspace/ds/ds_salary_proj/chromedriver"
+
+df = gs.get_jobs("data_scientist",5,False,path,5);
